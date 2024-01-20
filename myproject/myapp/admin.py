7150085476f93788261c5280f08df676f978a558
saumitraid <saumitraid.das@gmail.com>
2024-01-20 17:28:23 +0530

    Django Admin

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from . models import Doctor
 # Register your models here.
+
+@admin.register(Doctor)
+class DoctorAdmin(admin.ModelAdmin):
+    list_display=('dname','ddegree')
+
+
--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -6,3 +6,8 @@ from django.contrib.auth.models import AbstractUser
 class MyUser(AbstractUser):
     mobile=models.CharField(max_length=12, verbose_name='Contact Number')
     address=models.TextField(verbose_name='Address')
+
+class Doctor(models.Model):
+    did=models.AutoField(primary_key=True)
+    dname=models.CharField(max_length=255, verbose_name='Doctor name')
+    ddegree=models.CharField(max_length=255)
